7a4204a84b2f91703774943bfdab8af43a38e1ce
mohammedkaddur <mohmad9853ff@gmail.com>
2022-09-27 19:14:15 +0300

    Upload My Website

---
+++ js/info.js
@@ -0,0 +1,12 @@
+// let arr = "Welcome In My Website for Web Site";
+// let i = 0,
+//   text = "";
+// let div = document.querySelector("div");
+// function write() {
+//   if (i < arr.length) {
+//     div.innerHTML += text.charAt(i);
+//     i++;
+//     setTimeout(write, 100);
+//   }
+// }
+// write();
