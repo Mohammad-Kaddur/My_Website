2b6f2fbbbeb27f13d941d4cd6aa8db068b9797d1
mohammedkaddur <mohmad9853ff@gmail.com>
2023-01-30 00:05:56 +0300

    Update HTML File

--- js/info.js
+++ js/info.js
@@ -1,27 +1,27 @@
-let download = document.getElementById("download-cv");
-console.log(download);
-download.onclick = function () {
-  getPdf();
-  // download.herf =
-  //   "https://drive.google.com/file/d/1zPFyll-xNlYhiHIY0kRY5G8XETQQPNd5/view?usp=drivesdk";
-};
+// let download = document.getElementById("download-cv");
+// console.log(download);
+// download.onclick = function () {
+//   getPdf();
+//   download.herf =
+//     "https://drive.google.com/file/d/1zPFyll-xNlYhiHIY0kRY5G8XETQQPNd5/view?usp=drivesdk";
+// };
 
-function getPdf() {
-  let getPdf = new XMLHttpRequest();
-  getPdf.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      let getResponseText = JSON.parse(this.responseText);
-      console.log(getResponseText);
-      let questionCounts = getResponseText.length;
-    }
-  };
-  getPdf.open(
-    "GET",
-    "https://drive.google.com/file/d/1zPFyll-xNlYhiHIY0kRY5G8XETQQPNd5/view?usp=drivesdk' from origin 'http://127.0.0.1:5500",
-    true
-  );
-  getPdf.send();
-}
+// function getPdf() {
+//   let getPdf = new XMLHttpRequest();
+//   getPdf.onreadystatechange = function () {
+//     if (this.readyState === 4 && this.status === 200) {
+//       let getResponseText = JSON.parse(this.responseText);
+//       console.log(getResponseText);
+//       let questionCounts = getResponseText.length;
+//     }
+//   };
+//   getPdf.open(
+//     "GET",
+//     "https://drive.google.com/file/d/1zPFyll-xNlYhiHIY0kRY5G8XETQQPNd5/view?usp=drivesdk'",
+//     true
+//   );
+//   getPdf.send();
+// }
 
 // fetch(
 //   "https://drive.google.com/file/d/1zPFyll-xNlYhiHIY0kRY5G8XETQQPNd5/view?usp=drivesdk' from origin 'http://127.0.0.1:5500"
